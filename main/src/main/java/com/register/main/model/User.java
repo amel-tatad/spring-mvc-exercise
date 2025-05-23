8629b3c33ec76e038ce964f03d8041f060b01b59
Amel Noah Tatad <amel.tatad@awsys-i.com>
2025-05-23 10:03:43 +0800

    Fix errors in model

--- main/src/main/java/com/register/main/model/User.java
+++ main/src/main/java/com/register/main/model/User.java
@@ -2,44 +2,82 @@ package com.register.main.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @NotBlank(message = "First name is required")
+    @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
 
     @NotBlank(message = "Last name is required")
+    @Column(name = "last_name", nullable = false, length = 100)
     private String lastName;
 
-    private LocalDate dateOfBirth;
+    @Column(name = "dob")
+    private LocalDate dob;
 
     @NotBlank(message = "Gender is required")
+    @Column(name = "gender", nullable = false, length = 10)
     private String gender;
 
     @NotBlank(message = "Username is required")
     @Size(min = 4, message = "Username must be at least 4 characters")
+    @Column(name = "username", nullable = false, length = 50, unique = true)
     private String username;
 
     @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters")
+    @Column(name = "password", nullable = false)
     private String password;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    private String phoneNumber;
+    @Column(name = "phone", length = 15) // @NotBlank might be added based on requirements
+    private String phone;
 
-    private boolean termsAndConditions;
+    @Column(name = "terms", nullable = false)
+    private boolean terms;
 
-    // Default constructor (required by Spring)
     public User() {
+        
     }
 
+    public User(String firstName, String lastName, LocalDate dob, String gender, String username, String password, String email, String phone, boolean terms) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.gender = gender;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.terms = terms;
+    }
+
+    public Long getId() {
+    	return id;
+    }
+    
+    public void setId(Long id) {
+    	this.id = id;
+    }
+    
     public String getFirstName() {
         return firstName;
     }
@@ -56,12 +94,12 @@ public class User {
         this.lastName = lastName;
     }
 
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
+    public LocalDate getDob() {
+        return dob;
     }
 
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
     }
 
     public String getGender() {
@@ -96,19 +134,19 @@ public class User {
         this.email = email;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setPhoneNumber(String phone) {
+        this.phone = phone;
     }
 
-    public boolean isTermsAndConditions() {
-        return termsAndConditions;
+    public boolean isTerms() {
+        return terms;
     }
 
-    public void setTermsAndConditions(boolean termsAndConditions) {
-        this.termsAndConditions = termsAndConditions;
+    public void setTerms(boolean terms) {
+        this.terms = terms;
     }
 }
